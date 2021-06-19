2dcda24478b13413b445b31f2cdbc173f3b41788
rantegu <leroyoy7@gmail.com>
2021-06-19 15:44:05 +0200

    fontawesome and translation

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.from(".uno", {
   opacity: 0,
-  x: -500,
+  x: 0,
+  duration: 1,
 });
 
 gsap.to(".uno", {
@@ -16,12 +17,43 @@ gsap.to(".uno", {
 
 gsap.from(".due", {
   opacity: 0,
-  x: 500,
+  x: 0,
+  duration: 1,
 });
 
 gsap.to(".due", {
   scrollTrigger: {
     trigger: ".due",
+    start: "bottom bottom",
+  },
+  opacity: 1,
+  x: 0,
+});
+
+gsap.from(".tre", {
+  opacity: 0,
+  x: 0,
+  duration: 1,
+});
+
+gsap.to(".tre", {
+  scrollTrigger: {
+    trigger: ".tre",
+    start: "bottom bottom",
+  },
+  opacity: 1,
+  x: 0,
+});
+
+gsap.from(".quattro", {
+  opacity: 0,
+  x: 0,
+  duration: 1,
+});
+
+gsap.to(".quattro", {
+  scrollTrigger: {
+    trigger: ".quattro",
     start: "bottom bottom",
   },
   opacity: 1,
